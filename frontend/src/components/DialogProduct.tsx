6c13dce8437c6039cc01c4665407ada4c10279fe
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-15 17:18:45 -0400

    more checks to catch errors

--- frontend/src/components/DialogProduct.tsx
+++ frontend/src/components/DialogProduct.tsx
@@ -26,14 +26,20 @@ function DialogProduct({ product }: { product: Product }) {
       <div className="border-t-1 border-[#83667e] border-solid w-full"></div>
       {/* price and purchase link */}
       <div className="flex justify-between w-full mt-5">
-        <div className="font-bold text-2xl relative shrink-0 text-[#fca4c0]">
-          <p>${product?.price}</p>
-          {product?.unit_price != null && Number(product?.unit_price) > 0 && (
-            <p className="text-nowrap font-normal text-[16px] mt-2 tracking-wide">
-              ${Number(product?.unit_price).toFixed(2)} per 100mL
-            </p>
-          )}
-        </div>
+        {product?.price != null && Number(product?.price) > 0 ? (
+          <div className="font-bold text-2xl relative shrink-0 text-[#fca4c0]">
+            <p>${product?.price}</p>
+            {product?.unit_price != null && Number(product?.unit_price) > 0 && (
+              <p className="text-nowrap font-normal text-[16px] mt-2 tracking-wide">
+                ${Number(product?.unit_price).toFixed(2)} per 100mL
+              </p>
+            )}
+          </div>
+        ) : (
+          <div className="font-bold text-2xl relative shrink-0 text-[#fca4c0]">
+            <p>Price not available</p>
+          </div>
+        )}
         <div className="relative shrink-0">
           <a
             href={`https://www.amazon.ca/dp/${product?.asin}`}
--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -17,17 +17,22 @@ const discount = originalProduct?.unit_price != null && product?.unit_price != n
             <Image alt="product image" className="block object-contain" src={product?.image_url ?? ''} fill />
         </div>
         <div className='border-t-2 border-[#b398ae] border-solid w-full'></div>
-        <div className="m-4 font-medium text-[#83667e] text-lg tracking-tight w-5/6 h-30 overflow-hidden">
+        <div className="m-4 font-medium text-[#83667e] text-lg tracking-tight w-5/6 h-32 overflow-hidden">
             <p>{product?.title ?? ''}</p>
         </div>
+        {product?.similarity_score != null && (
+          <div>
+            <p className='text-[#fca4c0] text-sm tracking-tight'>{product?.similarity_score * 100}% similar</p>
+          </div>
+        )}
         <div className='grid justify-items-end w-full'>
         <Dialog.Trigger asChild>
           <button className="text-[#83667e] m-5 mb-4 font-medium tracking-tight bg-white box-border content-stretch flex flex-col gap-[10px] h-[40px] items-end justify-center border-2 border-[#83667e] border-solid rounded-4xl p-5 hover:bg-[#83667e] hover:text-white hover:cursor-pointer" onClick={onCompareClick}>Compare Products</button>
         </Dialog.Trigger>
         </div>
-        {discount && (
+        {discount && Number(discount) > 0 && (
           <div className="absolute -top-4 -right-4 flex items-center justify-center bg-[#fca4c0] text-white text-xl font-bold p-3 border-1 border-[#83667E] aspect-square rounded-full -rotate-22">
-            {discount}%
+            -{discount}%
           </div>
         )}
     </div>
