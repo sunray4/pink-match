78bec90e937447a38206fbe95c0c018383c08876
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-14 20:35:19 -0400

    finish refactoring how-it-works section in home page complete

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeatureCard from "@/components/FeatureCard";
 import SearchInput from "@/components/SearchInput";
 import Image from "next/image";
 
@@ -5,7 +6,7 @@ export default function Home() {
   return (
     <div className="">
       {/* top hero */}
-      <section className="">
+      <section>
         <div className="mx-14">
           <div className="relative w-full h-[80vh] overflow-hidden">
             <Image
@@ -16,17 +17,32 @@ export default function Home() {
               priority
             />
             <div className="relative z-20 ml-14 mt-40">
-              <p className="font-bold [text-shadow:#ffffff_0px_0px_46.042px] text-[#83667e] text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[100px] tracking-[-2px] sm:tracking-[-3px] md:tracking-[-3.84px]">No Pink Tax</p>
-              <div className="font-cormorant text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[128px] tracking-[-2px] sm:tracking-[-3px] md:tracking-[-3.84px]">
-                <span className="text-white">With </span>
-                <span className="text-[#fca4c0]">PinkMatch</span>
+              <p className="font-bold [text-shadow:#ffffff_0px_0px_46.042px] text-[#83667e] text-6xl lg:text-7xl xl:text-[6.25rem] tracking-[-0.038em]">No Pink Tax</p>
+              <div className="font-cormorant text-7xl md:text-[8rem] tracking-[-0.055em]">
+                <span className="text-white text-shadow-[0_0_20px_rgb(255_255_255_/_0.9)]">With </span>
+                <span className="text-[#fca4c0] text-shadow-[0_0_20px_rgb(252_164_192_/_0.9)]">PinkMatch</span>
               </div>
               <SearchInput />
             </div>
             <div className="absolute right-14 bottom-14 z-20">
-              <p className="text-[#83667e] tracking-[-0.5px] sm:tracking-[-0.72px]">Let us help you find an alternative.</p>
+              <p className="text-[#83667e] text-xl tracking-tight sm:tracking-[ -0.04em ]">Let us help you find an alternative.</p>
             </div>
           </div>
+        </div>
+      </section>
+
+      <div className="relative my-7 mb-8 mx-14 h-2">
+        <Image alt="line" src="/line.svg" fill />
+      </div>
+
+      {/* how-it-works section */}
+      <section className="mx-14 bg-[#ffe8f0] p-14" id="how-it-works">
+        <p className="text-[#83667e] font-bold font-cormorant text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[75px] tracking-[-1px] sm:tracking-[-2px] md:tracking-[-3.75px]">Search. Match. Compare.</p>
+        {/* Three feature cards */}
+        <div className="flex box-border content-stretch flex-col lg:flex-row items-center justify-between mt-10">
+          <FeatureCard description="Start by entering the product you want, from razors to shampoo, and we'll begin the hunt for fairer alternatives." imgSrc="/frame-inspect.png" />
+          <FeatureCard description="We'll find nearly identical men's or gender-neutral products that deliver the same quality at a lower cost." imgSrc="/folder-match.png" />
+          <FeatureCard description="View both options side by side to compare the products and see the real impact of the pink tax." imgSrc="/compare-arrows.png" />
         </div>
       </section>
     </div>
---
+++ frontend/src/components/FeatureCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface FeatureCardProps {
+  description: string;
+  imgSrc: string;
+}
+
+function FeatureCard({ description, imgSrc }: FeatureCardProps) {
+
+  return (
+    <div className="bg-white box-border content-stretch flex flex-col h-[52vh] items-center relative rounded-4xl shrink-0 w-full max-w-[22rem] lg:w-[23rem] cursor-pointer transition-all duration-300 hover:transform hover:-translate-y-2 hover:shadow-[0px_12px_20px_0px_rgba(0,0,0,0.15)]">
+        <div aria-hidden="true" className="absolute border-2 border-[#b398ae] border-solid inset-0 pointer-events-none rounded-4xl shadow-[0px_6px_7px_0px_rgba(0,0,0,0.25)] transition-all duration-300" />
+        <div className="relative shrink-0 m-12 size-[13.7rem]">
+            <Image alt="Search inspection icon" className="block max-w-none size-full" src={imgSrc} width={219} height={219} />
+        </div>
+        <div className="px-4 font-medium shrink-0 text-[#b398ae] text-lg tracking-tight w-72">
+            <p>{description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FeatureCard
