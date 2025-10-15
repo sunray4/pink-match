b046bf310f875b847104a9b8889e12ad87e2a964
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-14 21:17:30 -0400

    why us section of home page complete

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="">
       {/* top hero */}
-      <section>
-        <div className="mx-14">
+      <section className="mx-14">
+        {/* <div className="mx-14"> */}
           <div className="relative w-full h-[80vh] overflow-hidden">
             <Image
               src="/pink-hero.webp"
@@ -28,7 +28,7 @@ export default function Home() {
               <p className="text-[#83667e] text-xl tracking-tight sm:tracking-[ -0.04em ]">Let us help you find an alternative.</p>
             </div>
           </div>
-        </div>
+        {/* </div> */}
       </section>
 
       <div className="relative my-7 mb-8 mx-14 h-2">
@@ -37,12 +37,45 @@ export default function Home() {
 
       {/* how-it-works section */}
       <section className="mx-14 bg-[#ffe8f0] p-14" id="how-it-works">
-        <p className="text-[#83667e] font-bold font-cormorant text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[75px] tracking-[-1px] sm:tracking-[-2px] md:tracking-[-3.75px]">Search. Match. Compare.</p>
+        <p className="text-[#83667e] font-bold font-cormorant text-4xl lg:text-5xl xl:text-[75px] tracking-[-2px] md:tracking-[-3.75px]">Search. Match. Compare.</p>
         {/* Three feature cards */}
         <div className="flex box-border content-stretch flex-col lg:flex-row items-center justify-between mt-10">
           <FeatureCard description="Start by entering the product you want, from razors to shampoo, and we'll begin the hunt for fairer alternatives." imgSrc="/frame-inspect.png" />
           <FeatureCard description="We'll find nearly identical men's or gender-neutral products that deliver the same quality at a lower cost." imgSrc="/folder-match.png" />
           <FeatureCard description="View both options side by side to compare the products and see the real impact of the pink tax." imgSrc="/compare-arrows.png" />
+        </div>
+      </section>
+
+      <div className="relative my-7 mb-8 mx-14 h-2">
+        <Image alt="line" src="/line.svg" fill />
+      </div>
+
+      {/* why section */}
+      <section className="mx-14" id="why">
+        <div className="relative w-full h-[80vh] overflow-hidden">
+          <Image
+            src="/white-hero.webp"
+            alt="white hero"
+            fill
+            className="object-cover opacity-59 filter blur-[2px]"
+            priority
+          />
+          <div className="relative z-20 flex justify-center pt-30">
+            <div className="text-center">
+              <p className="font-cormorant mb-10 text-[#fca4c0] text-6xl lg:text-7xl xl:text-[8rem] tracking-[-0.023em] md:tracking-[-0.031em] lg:tracking-[-0.05em]">Why We Care</p>
+              <div className="max-w-5xl text-[#83667e] text-[1.125rem] md:text-[1.5rem] tracking-[-0.034em] mt-6">
+                <p>
+                  Everyday products marketed to women often cost more than nearly identical men&apos;s or gender-neutral versions. This unfair price gap, known as the
+                  <span className="font-bold"> pink tax</span>
+                  , means women are charged more for basics like razors, deodorant, and shampoo simply because of branding.
+                </p>
+                <p className="mt-4">We created this web app to help remove that imbalance. Our aim is to help people save money and also expose a hidden markup that too often goes unquestioned.</p>
+                <p className="mt-4">
+                  For us, <span className="font-bold">PinkMatch</span> means fairness, choice and making the cost of living more equitable.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
