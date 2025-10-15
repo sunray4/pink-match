4955970d931b9714e585fc1cc58ad009fc0fe314
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-15 18:35:26 -0400

    redesigning product modal but not complete yet

--- frontend/src/components/CompareDialog.tsx
+++ frontend/src/components/CompareDialog.tsx
@@ -1,40 +1,52 @@
-import React from 'react'
+import React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
-import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
-import { Product } from '@/utils/models';
-import DialogProduct from './DialogProduct';
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import { Product } from "@/utils/models";
+import DialogProduct from "./DialogProduct";
 
 interface CompareModalProps {
   originalProduct: Product;
   newProduct: Product;
 }
 
-function CompareDialog( {originalProduct, newProduct} : CompareModalProps) {
+function CompareDialog({ originalProduct, newProduct }: CompareModalProps) {
   return (
     <Dialog.Portal>
-        <Dialog.Overlay className='fixed inset-0 bg-black/60 animate-[overlayShow_150ms_cubic-bezier(0.16,1,0.3,1)]' />
-        <Dialog.Content 
-            className='flex flex-col border-1 border-[#b398ae] border-solid bg-white fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] h-[90vh] rounded-2xl shadow-lg focus:outline-none overflow-visible'>
-            <VisuallyHidden.Root>
-                <Dialog.Title>Compare Products</Dialog.Title>
-            </VisuallyHidden.Root>
-            <Dialog.Close asChild>
-                <button
-                    className="absolute right-4 top-4 z-20 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 transition-colors"
-                    aria-label="Close modal"
-                >
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M18 6L6 18M6 6L18 18" stroke="#83667e" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                </button>
-            </Dialog.Close>
-            <div className="overflow-auto p-14 flex justify-evenly items-start gap-6 w-full">
-                <DialogProduct product={originalProduct} />
-                <DialogProduct product={newProduct} />
-            </div>
-        </Dialog.Content>
+      <Dialog.Overlay className="fixed inset-0 bg-black/60 animate-[overlayShow_150ms_cubic-bezier(0.16,1,0.3,1)]" />
+      <Dialog.Content className="flex flex-col border-1 border-[#b398ae] border-solid bg-white fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] h-[90vh] rounded-2xl shadow-lg focus:outline-none overflow-visible">
+        <VisuallyHidden.Root>
+          <Dialog.Title>Compare Products</Dialog.Title>
+        </VisuallyHidden.Root>
+        <Dialog.Close asChild>
+          <button
+            className="absolute right-4 top-4 z-20 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 transition-colors"
+            aria-label="Close modal"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 6L6 18M6 6L18 18"
+                stroke="#83667e"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </Dialog.Close>
+        {/* flex justify-evenly items-start gap-6 w-full */}
+        <div className="overflow-auto p-14 ">
+          <DialogProduct originalProduct={originalProduct} product={newProduct} />
+          {/* <DialogProduct product={newProduct} /> */}
+        </div>
+      </Dialog.Content>
     </Dialog.Portal>
-  )
+  );
 }
 
-export default CompareDialog
+export default CompareDialog;
--- frontend/src/components/DialogProduct.tsx
+++ frontend/src/components/DialogProduct.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Product } from "@/utils/models";
 import Image from "next/image";
+import DialogPrice from "./DialogPrice";
+import DialogDetails from "./DialogDetails";
 
-function DialogProduct({ product }: { product: Product }) {
+function DialogProduct({ originalProduct, product }: { originalProduct: Product; product: Product }) {
   return (
     <div className="flex flex-col items-center justify-center w-2/5">
       {/* product image */}
+      <div className="flex justify-evenly items-start gap-6 w-full">
+      <div className="relative p-3 bg-[#ffe8f0] border-2 border-[#b398ae] border-solid rounded-4xl flex justify-center items-center w-full aspect-square">
+        <div className="relative w-full h-full">
+          {originalProduct.image_url ? (
+            <Image
+              src={originalProduct.image_url}
+              alt="product image"
+              fill
+              className="object-contain"
+            />
+          ) : (
+            <div>No image available</div>
+          )}
+        </div>
+      </div>
       <div className="relative p-3 bg-[#ffe8f0] border-2 border-[#b398ae] border-solid rounded-4xl flex justify-center items-center w-full aspect-square">
         <div className="relative w-full h-full">
           {product.image_url ? (
@@ -20,68 +37,41 @@ function DialogProduct({ product }: { product: Product }) {
           )}
         </div>
       </div>
+      </div>
       {/* product details */}
       {/* product title */}
-      <div className="m-4 font-semibold font-cormorant text-[#83667e] text-2xl tracking-tight w-full overflow-hidden">
-        <p>{product?.title ?? ""}</p>
-      </div>
-      <div className="border-t-1 border-[#83667e] border-solid w-full"></div>
-      {/* price and purchase link */}
-      <div className="flex justify-between w-full mt-5">
-        {product?.price != null && Number(product?.price) > 0 ? (
-          <div className="font-bold text-2xl relative shrink-0 text-[#fca4c0]">
-            <p>${product?.price}</p>
-            {product?.unit_price != null && Number(product?.unit_price) > 0 && (
-              <p className="text-nowrap font-normal text-[16px] mt-2 tracking-wide">
-                ${Number(product?.unit_price).toFixed(2)} per 100mL
-              </p>
-            )}
-          </div>
-        ) : (
-          <div className="font-bold text-2xl relative shrink-0 text-[#fca4c0]">
-            <p>Price not available</p>
-          </div>
-        )}
-        <div className="relative shrink-0">
-          <a
-            href={`https://www.amazon.ca/dp/${product?.asin}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline text-xl font-semibold text-[#83667e] hover:text-[#fca4c0] transition-colors"
-          >
-            Purchase Here
-          </a>
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <div className="m-4 font-semibold font-cormorant text-[#83667e] text-2xl tracking-tight w-full overflow-hidden">
+          <p>{originalProduct?.title ?? ""}</p>
         </div>
+        <div className="m-4 font-semibold font-cormorant text-[#83667e] text-2xl tracking-tight w-full overflow-hidden">
+          <p>{product?.title ?? ""}</p>
+        </div>
+      </div>
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <div className="border-t-1 border-[#83667e] border-solid w-full" />
+        <div className="border-t-1 border-[#83667e] border-solid w-full" />
+      </div>
+      {/* price and purchase link */}
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <DialogPrice product={originalProduct} />
+        <DialogPrice product={product} />
       </div>
       {/* product description */}
-        <div className="text-[#83667e] w-full mt-7">
-          <p className="font-bold text-2xl">Product Description</p>
-          {product?.description ? (
-            <p className="tracking-tight">
-              {product?.description}
-            </p>
-          ) : (
-            <p className="tracking-tight">
-              No description available
-            </p>
-          )}
-        </div>
-        {/* product ingredients */}
-        <div className="text-[#83667e] w-full mt-7">
-          <p className="font-bold text-2xl">Ingredients</p>
-          {product?.ingredients && product.ingredients.length > 0 ? (
-            <p className="tracking-tight">{product.ingredients.join(", ")}</p>
-          ) : (
-            <p className="tracking-tight">No ingredients available</p>
-          )}
-        </div>
-        {/* product fragrances */}
-        {product?.fragrances && product.fragrances.length > 0 && (
-            <div className="text-[#83667e] w-full mt-7">
-              <p className="font-bold text-2xl">Fragrances</p>
-              <p className="tracking-tight">{product.fragrances.join(", ")}</p>
-            </div>
-          )}
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <DialogDetails title="Description" productInfo={originalProduct?.description} />
+        <DialogDetails title="Description" productInfo={product?.description} />
+      </div>
+      {/* product ingredients */}
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <DialogDetails title="Ingredients" productInfo={originalProduct?.ingredients ? originalProduct.ingredients.join(", ") : ""} />
+        <DialogDetails title="Ingredients" productInfo={product?.ingredients ? product.ingredients.join(", ") : ""} />
+      </div>
+      {/* product fragrances */}
+      <div className="flex justify-evenly items-start gap-6 w-full">
+        <DialogDetails title="Fragrances" productInfo={originalProduct?.fragrances ? originalProduct.fragrances.join(", ") : ""} />
+        <DialogDetails title="Fragrances" productInfo={product?.fragrances ? product.fragrances.join(", ") : ""} />
+      </div>
     </div>
   );
 }
